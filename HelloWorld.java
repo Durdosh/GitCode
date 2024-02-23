2cc1246fbf4caca78dde8f329917327fe9b0ac88
durdanaseidova <durdanasei@gmail.com>
2024-02-23 00:54:32 -0500

    Set Up

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         String hello = "Hello";
+
+        Scanner scan=new Scanner(System.in);
 
 
 
